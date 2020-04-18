c27d4dd74eb4bca653a8377d724a094fce191d3a
y0b007t <yogesh.bansal@walmartlabs.com>
2020-04-18 20:07:12 +0530

    Spring security basic authentication using JDBC

---
+++ guest-app/src/main/java/com/frankmoley/security/app/auth/LandonUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.frankmoley.security.app.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LandonUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public LandonUserDetailsService(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username).orElseThrow(()
+                -> new UsernameNotFoundException("can not find username" + username));
+        return new LandonUserPrincipal(user);
+    }
+}
---
+++ guest-app/src/main/java/com/frankmoley/security/app/auth/User.java
@@ -0,0 +1,45 @@
+package com.frankmoley.security.app.auth;
+
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User {
+
+    @Id
+    @Column(name = "USER_ID")
+    private long userId;
+    @Column(name = "USERNAME", nullable = false, unique = true)
+    private String username;
+    @Column(name = "PASSWORD")
+    private String password;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
